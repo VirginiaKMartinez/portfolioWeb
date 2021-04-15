41b1206e6f7ab93c4eafe4e7af74bca76a062034
VirginiaKMartinez <hola@virginiak.es>
2021-04-15 21:37:21 +0200

    haciendo el wireframe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles/wireframe.css'
+// import {underConstruction} from '../src/js/views/underConstruction';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <h1>Virginia Martínez Portfolio Web</h1>
         <h2>Under Construction</h2>
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,7 +24,18 @@ function App() {
           Linkedin
         </a>
         
-      </header>
+      </header> */}
+      <div className="portada"><span>Esta es mi portada</span></div>
+      <div className="aboutMe"><span>About me</span></div>
+      <div className="experiencia"><span>sta es mi experiencia</span>E</div>
+      <div className="estudios"><span>Mis estudios</span></div>
+      <div className="idiomas"><span>Idiomas</span></div>
+      <div className="techDesign"><span>Apartado Tech in love con Design</span></div>
+      <div className="contact"><span>Links a "GitHub","Linkedin",...</span></div>
+
+
+      
+
     </div>
   );
 }
