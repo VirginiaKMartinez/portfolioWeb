bfcb54de0d72635376d9883865e321065067c780
VirginiaKMartinez <hola@virginiak.es>
2021-04-18 10:42:18 +0200

    adding style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import './styles/wireframe.css'
+
 
 
 // import {underConstruction} from '../src/js/views/underConstruction';
--- src/js/components/LottieWoderwoman.jsx
+++ src/js/components/LottieWoderwoman.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import LottieAnimation from '../../lottie/Lottie.js';
 import wonderWoman from '../../lottie/superhero_01.json';
+import '../../styles/underConstruction.css';
 
 
 const LottieWonderWoman = () => {
 
     return ( 
-         <div className='wrapper'>
-            <h1>Page Under Construction</h1>
-            <h3>Hi, I'm Virginia and I'm a FrontEnd Developer and UX / UI Designer</h3>
-            <div>
+         <div className='wrapperBody'>
+            <h1 className="title">Page Under Construction</h1>
+            <h3 className="helloText">Hi, I'm Virginia and I'm a FrontEnd Developer and UX / UI Designer</h3>
+            <div className="descriptText">
                 <span>I'm working very hard to design and develop my web portfolio with React.js, meanwhile, you can enjoy Wonder Woman watching over the sky.
                     An animation that I have created from After Effects with the Lottie plugin and implemented in my code.
                 </span>
                 <LottieAnimation lotti={wonderWoman} height={300} width={300} />
             </div>
-           <span>You can also visit my Linkedin profile</span>
+           <span>You can also visit my Linkedin profile </span>
            <i className="fab fa-linkedin"></i>
            <br></br>
            <span>Or you can Download my CV in pdf</span>
            <button>Download</button>
-           <span></span>
+          
         </div>
     )
 }
