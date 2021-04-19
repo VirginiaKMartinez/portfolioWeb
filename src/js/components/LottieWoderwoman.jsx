55123a0731395636e79e5150d7a7bca04893dc59
VirginiaKMartinez <hola@virginiak.es>
2021-04-19 21:01:17 +0200

    implementing figma design

--- src/js/components/LottieWoderwoman.jsx
+++ src/js/components/LottieWoderwoman.jsx
@@ -9,11 +9,12 @@ const LottieWonderWoman = () => {
     return ( 
          <div className='wrapperBody'>
             <div className="headBlock">
-                
-                <h1 className="title"><i class="fas fa-drafting-compass"></i><br></br>Under Construction</h1>
+                <i className="fas fa-exclamation-triangle"></i>
+                <h1 className="titleH1">Portfolio</h1>
+                <h2 className="titleH2">Under Construction</h2>
             </div>
             <div className="iconsFigmaLoveCode">
-                <h3 className="helloText">Hi I'm Virginia and I'm a FrontEnd Developer and UX / UI Designer</h3>
+                <h3 className="helloText">Hi, I'm Virginia!<br></br>I'm a FrontEnd Developer<br></br>and a UX/UI Designer</h3>
                 <i className="fab fa-figma"></i>
                 <i className="fas fa-heart"></i>
                 <i className="fas fa-code"></i>
@@ -23,15 +24,15 @@ const LottieWonderWoman = () => {
             
             <div className="textAndLottie">
                 <LottieAnimation lotti={wonderWoman} height={300} width={300} />
-                <span className="descriptText">I'm working very hard to design and develop my web portfolio with React.js. Meanwhile, you can enjoy Wonder Woman watching over the sky.
+                <span className="longParagraph">I'm working very hard to design and develop my web portfolio with React.js. Meanwhile, you can enjoy Wonder Woman watching over the sky.
                     An animation that I have created with After Effects, Lottie plugin and implemented in my code.
                 </span>
             </div>
             <div className="infoContact">
-                <span className="longParagraph">You can also visit my Linkedin profile </span>
+                <span className="shortParagraph">You can also visit my Linkedin profile </span>
                 <i className="fab fa-linkedin"></i>
                 <br></br>
-                <span className="descriptText">Or you can Download my CV in pdf</span>
+                <span className="shortParagraph">Or you can Download my CV in pdf</span>
                 <br></br>
                 <button>Download</button>
             </div>
