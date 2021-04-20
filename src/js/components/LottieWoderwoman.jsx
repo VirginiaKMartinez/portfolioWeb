da5f62491cb453265ec66b60a11bacaa946eed45
VirginiaKMartinez <hola@virginiak.es>
2021-04-20 15:09:03 +0200

    working on the layout

--- src/js/components/LottieWoderwoman.jsx
+++ src/js/components/LottieWoderwoman.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import LottieAnimation from '../../lottie/Lottie.js';
 import wonderWoman from '../../lottie/superhero_01.json';
 import '../../styles/underConstruction.css';
+import rocket from '../../img/rocket.png';
+import figma from '../../img/figma.svg';
+import code from '../../img/code.svg'
 
 
 const LottieWonderWoman = () => {
@@ -12,17 +15,16 @@ const LottieWonderWoman = () => {
                 {/* <i className="fas fa-exclamation-triangle"></i> */}
                 <h1 className="titleH1">Portfolio</h1>
                 <h2 className="titleH2">Under Construction</h2>
-                <img src="/Users/virginiamartinezfranco/Desktop/portfolioWeb/src/img/rocket.png" alt="rocket_image"/>
+                <img className="rocketImg" src={rocket} alt="rocket_image"/>
             </div>
-            <div className="iconsFigmaLoveCode">
-                <h3 className="helloText">Hi, I'm Virginia!<br></br>I'm a FrontEnd Developer<br></br>and a UX/UI Designer</h3>
-                <i className="fab fa-figma"></i>
-                <i className="fas fa-heart"></i>
-                <i className="fas fa-code"></i>
-                
+            <div className="textIconsFigmaLoveCode">
+                <h3 className="helloText">Hi, I'm <b>Virginia</b>!<br></br>I'm a <b>FrontEnd Developer</b><br></br>and a <b>UX/UI Designer</b>.</h3>
+                <div className="designLoveCode">
+                    <img src={figma} alt="figma logo"/>
+                    <i className="fas fa-heart"></i>
+                    <img src={code} alt="figma logo"/>
+                </div>
              </div>
-            
-            
             <div className="textAndLottie">
                 <LottieAnimation lotti={wonderWoman} height={300} width={300} />
                 <span className="longParagraph">I'm working very hard to design and develop my web portfolio with React.js. Meanwhile, you can enjoy Wonder Woman watching over the sky.
