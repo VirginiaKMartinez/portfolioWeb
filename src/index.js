69502d5677be48bf9ca09d44311e25f37329d11c
VirginiaKMartinez <hola@virginiak.es>
2022-12-02 15:41:53 +0100

    subiendo cambios

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import UnderConstruction from './js/components/UnderConstruction';
+// import UnderConstruction from './js/components/UnderConstruction';
+import PortfolioMvp from './js/components/PortfolioMvp';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <UnderConstruction />
+    <PortfolioMvp />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/js/components/PortfolioMvp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PortfolioMvp = () => {
+    return (
+        <div>
+            <p> Coucou, comencemos el portfolio</p>
+        </div>
+    )
+};
+
+export default PortfolioMvp;
